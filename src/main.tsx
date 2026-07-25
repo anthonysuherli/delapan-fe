bba2a1b53035db8628439cf20094b92e382e17fb
Anthony Suherli <anthonysuherli@gmail.com>
2026-07-25 19:43:11 -0400

    feat(console): hub component, tiles, and styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./styles/tokens.css";
 import "./styles/base.css";
 import "./styles/layout.css";
 import "./styles/panels.css";
+import "./styles/console.css";
 import "./styles/canvas.css";
 import "./styles/tracking.css";
 import "./styles/motion.css";
